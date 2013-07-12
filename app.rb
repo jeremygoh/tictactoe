f8f46294c89a5a2dad4bf1871dc51fde40aecc2c
Jeremy Goh <jg485host@gmail.com>
2013-07-12 17:35:03 +0100

    up until game play, trying to have background images for marked squares'

---
+++ app.rb
@@ -0,0 +1,51 @@
+require 'sinatra/base'
+require './lib/cell.rb'
+require './lib/board.rb'
+require './lib/player.rb'
+require './lib/checker.rb'
+require './lib/game.rb'
+
+
+
+class App < Sinatra::Base
+enable :sessions
+
+@@number_of_players = 0
+
+@@game = Game.new
+
+get '/' do
+	erb :index
+end
+
+post '/sign_up' do
+	session[:player_name] = params[:player_name]
+	@@game.add_player(params[:player_name])
+	@@number_of_players +=1
+	redirect '/lobby'
+end
+
+get '/lobby' do
+	@number_of_players = @@number_of_players
+	erb :lobby
+end
+
+get "/reset_game" do
+	session.clear
+	@@number_of_players = 0
+	@@game = Game.new #need to figure out how to delete players?
+	redirect '/'
+end
+
+get "/game" do
+	@game=@@game
+	erb :game
+end
+
+post "/game" do
+	@@game.player_mark(session[:player_name], params[:cell_to_fill].to_i)
+	redirect '/game'
+end
+
+
+end
